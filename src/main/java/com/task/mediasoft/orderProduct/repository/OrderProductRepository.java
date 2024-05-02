9ab483e6c9564c39c55880f3e7ca1963fc441f21
Николай <raspaev.niko@mail.ru>
2024-05-03 00:59:30 +0400

    fix

--- src/main/java/com/task/mediasoft/orderProduct/repository/OrderProductRepository.java
+++ src/main/java/com/task/mediasoft/orderProduct/repository/OrderProductRepository.java
@@ -1,9 +1,8 @@
 package com.task.mediasoft.orderProduct.repository;
 
 import com.task.mediasoft.orderProduct.model.OrderProduct;
+import com.task.mediasoft.orderProduct.model.OrderProductId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface OrderProductRepository extends JpaRepository<OrderProduct, UUID> {
+public interface OrderProductRepository extends JpaRepository<OrderProduct, OrderProductId> {
 }
