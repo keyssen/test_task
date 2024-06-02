7536d0a6d0b484542423d1a3f845fe4a1c0f5bd8
Николай <raspaev.niko@mail.ru>
2024-06-03 00:21:41 +0400

    fix: S3 configuration

--- src/main/java/com/task/mediasoft/configuration/S3Config.java
+++ src/main/java/com/task/mediasoft/configuration/S3Config.java
@@ -2,7 +2,6 @@ package com.task.mediasoft.configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.task.mediasoft.configuration.properties.S3Properties;
@@ -12,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class S3 {
+public class S3Config {
 
     private final S3Properties s3Properties;
 
@@ -21,8 +20,8 @@ public class S3 {
         return AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(
                         new com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration(
-                                "s3.localhost.localstack.cloud:4566",
-                                Regions.EU_CENTRAL_1.getName()
+                                s3Properties.getServiceEndpoint(),
+                                s3Properties.getSigningRegion()
                         )
                 )
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(s3Properties.getAccessKeyId(), s3Properties.getSecretAccessKey())))
